3dd2de3de9ceffbc2eee963ee7c710311a68f66b
施俊羽 <tom19960222@gmail.com>
2018-08-15 12:04:46 +0800

    Add support for JSON

--- lib/index.ts
+++ lib/index.ts
@@ -4,17 +4,25 @@ const { StringDecoder } = require('string_decoder');
 
 const cachedRequest = CachedRequest(request);
 
-function CachedHTTPClient(...args: any[]){
+function CachedHTTPClient(options: any){
   const stringDecoder = new StringDecoder('utf-8');
 
   return new Promise((resolve, reject) => {
-    cachedRequest(...args, (err: any, response: request.Response) => {
+    cachedRequest(options, (err: any, response: request.Response) => {
       if(err) return reject(err);
       const cacheHeader: any = Array.isArray(response.headers['x-from-cache']) ? 
         response.headers['x-from-cache'][0] : response.headers['x-from-cache'];
       if(parseInt(cacheHeader) === 1) {
         response.body = stringDecoder.write(response.body);
         stringDecoder.end();
+
+        if(options.json) {
+          try { 
+            const result = JSON.parse(response.body); 
+            response.body = result;
+          }
+          catch(err) {} // Ignore JSON parse error.
+        }
       }
       return resolve(response);
     })
